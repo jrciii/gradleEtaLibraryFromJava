b63a7822591a2f3c6126c364affd4301e37e3962
jcooper <jcooper@gravyanalytics.com>
2017-11-28 15:16:55 -0500

    Adding foreign export on bro creates a panic

--- src/main/eta/Scratch/Scratch.hs
+++ src/main/eta/Scratch/Scratch.hs
@@ -3,4 +3,8 @@ module Scratch.Scratch where
 dude :: IO ()
 dude = putStrLn "Omg"
 
+bro :: String -> String
+bro x = x ++ ", Bro"
+
 foreign export java "@static eta.scratch.Scratch.dude" dude :: IO ()
+foreign export java "@static eta.scratch.Scratch.bro" bro :: String -> String
